d89d018b6496bf9ce45f96524deeece3e0707372
MikeNichols1371 <mnich13711@gmail.com>
2022-07-26 10:50:04 -0400

    Finished exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word, bool expected)
+        {
+            return word == null || word != word.Reverse() ? false : true;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("rotor", true)]
+        [InlineData("taco", false)]
+        [InlineData("civic", true)]
+        [InlineData("madam", true)]
+        [InlineData("hotpocket", false)]
+        [InlineData("", false)]
 
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            //Arrange
+            var wordSmith = new WordSmith();
+
+            //Act
+            var actual = wordSmith.IsAPalindrome(word, expected);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
